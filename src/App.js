3ce6c5e85ce0154f01ced97f7115020fe705c193
tushardahiya <tushardahiya84@gmail.com>
2021-04-15 18:42:30 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const emojiDictionary = {
+  "🍓": "Strawberry",
+  "🍌": "Banana",
+  "🥝": "Kiwi Fruit",
+  "🍕": "Pizza",
+  "🥥": "Coconut",
+  "🥦": "Broccoli",
+};
+
+const emojisWeKnow = Object.keys(emojiDictionary);
 
 function App() {
+  const [meaning, setMeaning] = useState("");
+
+  const emojiInputHandler = (event) => {
+    let userInput = event.target.value;
+    let meaning = emojiDictionary[userInput];
+    if (meaning === undefined) {
+      meaning = "We dont have this in our database";
+    }
+    setMeaning(meaning);
+  };
+
+  const emojiClickHandler = (emoji) => {
+    let meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>EMOJI INTERPRETER</h1>
+      <input onChange={emojiInputHandler} />
+      <div className="meaning">The meaning of this emoji is {meaning}</div>
+      <h3 style={{ padding: "10px" }}>emojis we know: </h3>
+      {emojisWeKnow.map((emoji) => (
+        <span
+          onClick={() => emojiClickHandler(emoji)}
+          style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+          key={emoji}
         >
-          Learn React
-        </a>
-      </header>
+          {emoji}
+        </span>
+      ))}
     </div>
   );
 }
